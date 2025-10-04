13ef223365141cd41e439898979d5d2e467c2cd4
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2025-10-04 21:57:56 +0200

    fixed install.sh for the last time

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "This script will install BriFBoy's dotfiles and DELETE the allready existi
 sleep 2
 echo "Do you want to continue? [y/n]"
 read continue
-if [ "$continue" != "y" ]; then
+if [ "$continue" != "y" ] && [ "$continue" != "" ]; then
   exit
 fi
 
@@ -23,6 +23,7 @@ fi
 #
 yayInstallation () {
   if ! command -v yay >/dev/null; then
+    echo "installing yay"
     sudo pacman -S --needed --noconfirm git base-devel
     git clone https://aur.archlinux.org/yay.git ~/yay
     cd ~/yay
@@ -37,7 +38,7 @@ stowDotfilesSetup() {
   if command -v stow >/dev/null; then
     rm -rf ~/.bashrc # removes the allready existing .bashrc file
     stow hyprland ghostty bash rofi waybar dunst neovim
-    [ "$docandy" = "y" ] && stow cava fastfetch
+    [ "$docandy" = "y" ] || [ "$docandy" = "" ] && stow cava fastfetch
   else
     exit; echo "stow not installed"
   fi
@@ -48,13 +49,6 @@ pywalSetup() {
   . "$HOME/.cache/wal/colors.sh"
   export background foreground color0 color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15
   envsubst < ~/.config/dunst/dunstrc.template > ~/.config/dunst/dunstrc
-}
-# Starts up the swww-daemon just too quickly set det wallpaer before killing it
-setDefaultWallpaper() {
-  swww-daemon & 
-  sleep 4
-  swww img ~/.dotfiles/default_wallpaper.jpg
-  killall swww-daemon 
 }
 
 echo "Do you want candy stuff? [y/n] (cava, fastfetch, etc)"
@@ -75,11 +69,9 @@ pywalSetup
 yay -S --needed --noconfirm hyprland hypridle hyprlock hyprshot wlogout ly waybar swww ttf-jetbrains-mono-nerd neovim ghostty dolphin firefox 
 
 # Installs the candy packages if needed
-if [ "$docandy" = "y" ]; then 
+if [ "$docandy" = "y" ] || [ "$docandy" = "" ]; then 
   yay -S --needed --noconfirm cava fastfetch
 fi
-
-setDefaultWallpaper 
 
 # Starting the systemd service for the display manager
 sudo systemctl enable ly.service
