e2b39969a4ddd4e4a6c837b735db1b26d098827b
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2025-11-10 21:31:51 +0100

    update

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -8,3 +8,5 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.opt.linespace = 4
+
+vim.cmd("set clipboard+=unnamedplus")
--- neovim/.config/nvim/lua/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -6,7 +6,7 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = { "lua_ls", "jdtls", "ts_ls", "cssls", "tailwindcss" },
+			ensure_installed = { "lua_ls", "jdtls", "ts_ls", "cssls", "tailwindcss", "clangd" },
 			automatic_enable = {
 				exclude = {
 					"jdtls",
