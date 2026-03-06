b31d8113247b9fb2fdd9228ffec1f62966c602d5
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2026-03-06 14:49:28 +0100

    made picker my style

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -14,24 +14,29 @@ return {
 			},
 			sources = {
 				files = {
+					hidden = true,
 					exclude = {
 						"**/.git",
 						"**/node_modules",
 						"**/dist",
 						"**/build",
 					},
-					hidden = true,
 				},
 			},
 		},
 		explorer = {
 			enabled = true,
+			sources = {
+				files = {
+					hidden = true,
+				},
+			},
 		},
 		statuscolumn = { enabled = true },
 		indent = { enabled = true },
+		dashboard = { enabled = true },
 
 		bigfile = { enabled = false },
-		dashboard = { enabled = false },
 		input = { enabled = false },
 		notifier = { enabled = false },
 		quickfile = { enabled = false },
@@ -41,8 +46,6 @@ return {
 	},
 	keys = {
 		-- Top Pickers & Explorer
-		{ "<leader><space>", function() require("snacks").picker.smart() end, desc = "Smart Find Files" },
-		{ "<leader>,", function() require("snacks").picker.buffers() end, desc = "Buffers" },
 		{ "<leader>:", function() require("snacks").picker.command_history() end, desc = "Command History" },
 		{ "<leader>n", function() require("snacks").picker.notifications() end, desc = "Notification History" },
 		{ "<leader>e", function() require("snacks").explorer() end, desc = "File Explorer" },
@@ -50,11 +53,6 @@ return {
 		-- find
 		{ "<leader>fb", function() require("snacks").picker.buffers() end, desc = "Buffers" },
 		{ "<leader>fg", function() require("snacks").picker.grep() end, desc = "Find Grep" },
-		{
-			"<leader>fc",
-			function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end,
-			desc = "Find Config File",
-		},
 		{ "<leader>ff", function() require("snacks").picker.files() end, desc = "Find Files" },
 		{ "<leader>fp", function() require("snacks").picker.projects() end, desc = "Projects" },
 		{ "<leader>fr", function() require("snacks").picker.recent() end, desc = "Recent" },
