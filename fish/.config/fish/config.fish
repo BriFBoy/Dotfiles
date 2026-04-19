bc78197a7d3e7ce4bde43a398d16d32446d1f1b9
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2026-04-19 11:20:21 +0200

    added starhip w/ promt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,18 @@
-
 # Installs Starship if not install
 if not type -q starship
   echo "Starship not found"
   curl -sS https://starship.rs/install.sh | sh
 end
+
+# Sources the Pywal colors
 source ~/.cache/wal/colors.fish
 
+# Custom keybinds
+bind \cy accept-autosuggestion
 
+# Eye candy
 fastfetch
 
 # Enables Starship
 starship init fish | source
-
+enable_transience
