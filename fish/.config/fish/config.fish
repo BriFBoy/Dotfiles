f8a957319e56687cae73f9b1f86d9da80c5a9e26
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2026-04-20 08:59:06 +0200

    updated fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,21 @@
-# Installs Starship if not install
-if not type -q starship
-  echo "Starship not found"
-  curl -sS https://starship.rs/install.sh | sh
+if status is-interactive
+    # Installs Starship if not install
+    if not type -q starship
+        echo "Starship not found"
+        curl -sS https://starship.rs/install.sh | sh
+    end
+end
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+
+## Eye candy
+# overwrites the fish greeting
+function fish_greeting
+    fastfetch
 end
 
 # Sources the Pywal colors
@@ -10,8 +24,36 @@ source ~/.cache/wal/colors.fish
 # Custom keybinds
 bind \cy accept-autosuggestion
 
-# Eye candy
-fastfetch
+## Useful function
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Copy DIR1 DIR2
+function copy
+    set count (count $argv | tr -d \n)
+    if test "$count" = 2; and test -d "$argv[1]"
+        set from (echo $argv[1] | trim-right /)
+        set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
+end
+
+## Useful aliases
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons' # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'" # show only dotfiles
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
 
 # Enables Starship
 starship init fish | source
