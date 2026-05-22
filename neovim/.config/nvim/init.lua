41a127e3641fa7071994c619e0bfaabf395928cc
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2026-05-22 16:04:55 +0200

    split lsp configs to new file

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -67,29 +67,7 @@ for _, lang in pairs(languages) do
 end
 
 -- Lsp
-vim.lsp.config["lua_ls"] = {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = {
-					"vim",
-				},
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-}
-vim.lsp.config("jedi-language-server", {
-	cmd = { "jedi-language-server" },
-	filetypes = { "python" },
-	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", ".git" },
-})
+require("config.lsp")
 
 -- Custom commands
 vim.api.nvim_create_user_command(
